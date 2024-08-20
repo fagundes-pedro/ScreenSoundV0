48ec99e597ccddc5374befdf593223b5780cbfcc
fagundes-pedro <pe.fagundes@gmail.com>
2024-08-20 01:13:59 +0100

    add Endpoints (Get and Delete) and DTO for Genero

---
+++ ScreenSound.API/Endpoints/GenerosExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScreenSound.API.Requests;
+using ScreenSound.API.Response;
+using ScreenSound.Banco;
+using ScreenSound.Modelos;
+using ScreenSound.Shared.Modelos.Modelos;
+
+namespace ScreenSound.API.Endpoints;
+
+public static class GenerosExtensions
+{
+    public static void AddEndPointGeneros (this WebApplication app)
+    {
+        app.MapGet("/Generos", ([FromServices] DAL <Genero> dal) => {
+            var listaGeneros = EntityListToResponseList(dal.Listar());
+            return Results.Ok(listaGeneros);
+        });
+        app.MapDelete("/Gneros", ([FromServices] DAL<Genero> dal, int id) =>
+        {
+            var genero = dal.RecuperarPor(g => g.Id.Equals(id));
+            if (genero is null)
+            {
+                return Results.NotFound();
+            }
+            dal.Deletar(genero);
+            return Results.NoContent();
+        });
+    }
+    private static ICollection<GeneroResponse> EntityListToResponseList(IEnumerable<Genero> listaDeGeneros)
+    {
+        return listaDeGeneros.Select(g => EntityToResponse(g)).ToList();
+    }
+
+    private static GeneroResponse EntityToResponse(Genero genero)
+    {
+        return new GeneroResponse(genero.Id, genero.Nome!, genero.Descricao);
+    }
+}
